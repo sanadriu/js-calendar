103b316e5f7480e69dc3244eca8cb2f74bd23f4e
PildoritaGalactica <skualed@gmail.com>
2021-07-19 22:56:57 +0200

    Updated the size for the cell number

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import insertCalendarCells from "./views/insertCalendarCells.js";
 import setOffsetCalendarCells from "./views/setOffsetCalendarCells.js";
 
-insertCalendarCells(2021, 7);
-setOffsetCalendarCells(2021, 7);
+let month = 5;
+let year = 2021;
+insertCalendarCells(year, month);
+setOffsetCalendarCells(year, month);
