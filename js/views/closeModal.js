07636642ed5edb09ad59627cd1110d2b31c379c9
sanadriu <skualed@gmail.com>
2021-07-20 17:55:05 +0200

    Fixed modal styles

--- js/views/closeModal.js
+++ js/views/closeModal.js
@@ -1,6 +1,5 @@
 export default function closeModal() {
-    const modal = document.querySelector("#modal");
+	const modal = document.querySelector("#modal");
 
-
-    if (modal !== null) modal.remove();
+	if (modal !== null) modal.remove();
 }
