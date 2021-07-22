2e03d721ae2e733aaf5df3218dd54a21fb11248b
sanadriu <skualed@gmail.com>
2021-07-22 13:24:14 +0200

    Fixed the buttons for switching months

--- js/stuff.js
+++ js/stuff.js
@@ -1,56 +1,62 @@
 function getDaysInMonth(yearNum, monthNum) {
-  return new Date(yearNum, monthNum - 1, 0).getDate();
+	return new Date(yearNum, monthNum - 1, 0).getDate();
 }
 
 function getFirstDayOfMonth(yearNum, monthNum) {
-  return new Date(yearNum, monthNum - 1, 1);
+	return new Date(yearNum, monthNum - 1, 1);
 }
 
 function modifyCalendarCells(yearNum, monthNum) {
-  const calendarCells = document.querySelectorAll(".calendar__cell--day");
+	const calendarCells = document.querySelectorAll(".calendar__cell--day");
 
-  const monthNumOfDays = getDaysInMonth(yearNum, monthNum);
-  const firstDayOfMonth = getFirstDayOfMonth(yearNum, monthNum).getDay();
+	const monthNumOfDays = getDaysInMonth(yearNum, monthNum);
+	const firstDayOfMonth = getFirstDayOfMonth(yearNum, monthNum).getDay();
 
-  console.log(firstDayOfMonth);
-  if (firstDayOfMonth === 0) {
-    calendarCells[0].style.gridColumn = 7;
-  } else {
-    calendarCells[0].style.gridColumn = firstDayOfMonth;
-  }
+	console.log(firstDayOfMonth);
+	if (firstDayOfMonth === 0) {
+		calendarCells[0].style.gridColumn = 7;
+	} else {
+		calendarCells[0].style.gridColumn = firstDayOfMonth;
+	}
 
-  for (let i = 1; i < monthNumOfDays; i++) {
-    calendarCells[i].textContent = i + 1;
-    calendarCells[i].classList.remove("calendar__cell--disabled");
-  }
+	for (let i = 1; i < monthNumOfDays; i++) {
+		calendarCells[i].textContent = i + 1;
+		calendarCells[i].classList.remove("calendar__cell--disabled");
+	}
 }
 
 function clearCalendarCells() {
-  const calendarCells = document.querySelectorAll(".calendar__cell--day");
-  calendarCells.forEach((calendarCell) => {
-    calendarCell.innerHTML = null;
-    calendarCell.classList.add("calendar__cell--disabled");
-  });
+	const calendarCells = document.querySelectorAll(".calendar__cell--day");
+	calendarCells.forEach((calendarCell) => {
+		calendarCell.innerHTML = null;
+		calendarCell.classList.add("calendar__cell--disabled");
+	});
 }
 
 function updateCalendarCells(yearNum, monthNum) {
-  const calendarCells = document.querySelectorAll(".calendar__cell--day");
+	const calendarCells = document.querySelectorAll(".calendar__cell--day");
 
-  const monthNumOfDays = getDaysInMonth(yearNum, monthNum);
-  const firstDayOfMonth = getFirstDayOfMonth(yearNum, monthNum).getDay();
+	const monthNumOfDays = getDaysInMonth(yearNum, monthNum);
+	const firstDayOfMonth = getFirstDayOfMonth(yearNum, monthNum).getDay();
 
-  console.log(firstDayOfMonth);
-  if (firstDayOfMonth === 0) {
-    calendarCells[0].style.gridColumn = 7;
-  } else {
-    calendarCells[0].style.gridColumn = firstDayOfMonth;
-  }
+	console.log(firstDayOfMonth);
+	if (firstDayOfMonth === 0) {
+		calendarCells[0].style.gridColumn = 7;
+	} else {
+		calendarCells[0].style.gridColumn = firstDayOfMonth;
+	}
 
-  for (let i = 1; i < monthNumOfDays; i++) {
-    calendarCells[i].textContent = i + 1;
-    calendarCells[i].classList.remove("calendar__cell--disabled");
-  }
+	for (let i = 1; i < monthNumOfDays; i++) {
+		calendarCells[i].textContent = i + 1;
+		calendarCells[i].classList.remove("calendar__cell--disabled");
+	}
 }
 
 clearCalendarCells();
 modifyCalendarCells(2021, 6);
+
+document.addEventListener("click", (e) => {
+	if (e.target.matches("[data-date='every_date']")) {
+		showModal();
+	}
+});
