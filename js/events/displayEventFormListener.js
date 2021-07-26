7a0909bf693525b58cf45c35552eddf7dae0497d
sanadriu <skualed@gmail.com>
2021-07-26 19:00:14 +0200

    Saving changes

--- js/events/displayEventFormListener.js
+++ js/events/displayEventFormListener.js
@@ -5,7 +5,7 @@ export default function displayEventFormListener() {
 		if (document.querySelector("#modal-event-form") !== null) return;
 		if (!e.target.matches("[data-action~='display-event-form']") && !e.target.matches("[data-action~='display-event-form'] > *")) return;
 
-		if (e.target.matches("[data-action~='set-cell-date']")) return displayEventForm(sessionStorage.calendarYear, sessionStorage.calendarMonth, parseInt(e.target.dataset.day));
-		if (e.target.matches("[data-action~='set-current-date']")) return displayEventForm();
+		if (e.target.matches("[data-action~='set-cell-date']") || e.target.matches("[data-action~='set-cell-date'] > *")) return displayEventForm(sessionStorage.calendarYear, sessionStorage.calendarMonth, parseInt(e.target.dataset.day));
+		if (e.target.matches("[data-action~='set-current-date']") || e.target.matches("[data-action~='set-current-date'] > *")) return displayEventForm();
 	});
 }
