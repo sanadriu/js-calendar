292f53cfcced0501ceb29ef6aa7aa702ea654f0e
Alberto Chica <badyalberto@gmail.com>
2021-07-27 16:26:44 +0200

    responsive

--- js/view_modifiers/closeDayInfo.js
+++ js/view_modifiers/closeDayInfo.js
@@ -1,4 +1,7 @@
 export default function closeDayInfo() {
 	const dayInfo = document.querySelector("#day-info");
+	const calendarGrid = document.querySelector("#calendar-grid");
+
 	dayInfo.classList.add("day-info--hidden");
+	calendarGrid.classList.add("calendar-grid--full");
 }
--- js/view_modifiers/displayDayInfo.js
+++ js/view_modifiers/displayDayInfo.js
@@ -3,11 +3,13 @@ import clearEventCards from "./clearEventCards.js";
 import displayEventCards from "./displayEventCards.js";
 
 export default function displayDayInfo(year, month, day) {
+	const calendarGrid = document.querySelector("#calendar-grid");
 	const dayInfo = document.querySelector("#day-info");
 
 	const calendarYear = year;
 	const calendarMonthName = getMonthName(month);
 
+	calendarGrid.classList.remove("calendar-grid--full");
 	dayInfo.classList.remove("day-info--hidden");
 	dayInfo.querySelector("#day-info-date").textContent = `${calendarMonthName} ${day}, ${calendarYear}`;
 
