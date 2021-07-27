0a6363a2a2c93a2e86ac3207428fa02956cd47fa
Alberto Chica <badyalberto@gmail.com>
2021-07-27 09:34:15 +0200

    reminder

--- js/view_modifiers/displayCalendarEventNotification.js
+++ js/view_modifiers/displayCalendarEventNotification.js
@@ -9,7 +9,12 @@ export default function displayCalendarEventNotification(eventsReported) {
     for (let index = 0; index < eventsReported.length; index++) {
       if (!eventsReported[index].notification) {
         let array_date = eventsReported[index].initialDate.split("T");
-        eventInfo.innerHTML += `<div><strong>${eventsReported[index].title}</strong> <strong>Day: </strong>${array_date[0]}<strong>Hour:</strong> ${array_date[1]}</div>`;
+        eventInfo.innerHTML += `
+        <div class="p-1">
+        <strong>${eventsReported[index].title} </strong><br>
+        <span class="material-icons">event</span> ${array_date[0]}
+        <span class="material-icons">schedule</span> ${array_date[1]}
+        </div>`;
         eventsReported[index].notification = true;
         changeContent = true;
       }
